684182c70c1ee2f144fa2f3540e1d61438c2f4a8
Antoine M. Shepard <antoineshepard@pursuit.org>
2023-08-20 18:31:01 -0400

    app.js rewritten.

--- app.js
+++ app.js
@@ -22,10 +22,38 @@
 // // EXPORT
 // module.exports = app;
 
-const express = require('express');
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+
+// app.get('/', (req, res) => res.send('Project Artemis!'));
+
+// app.listen(port, () => console.log(`Express app running on port ${port}!`));
+
+const cors = require("cors");
+const express = require("express");
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => res.send('Project Artemis!'));
+// MIDDLEWARE
+app.use(cors());
+app.use(express.json());
 
-app.listen(port, () => console.log(`Express app running on port ${port}!`));
+// ROUTES
+app.get("/", (req, res) => {
+  res.send("Welcome to Artemis!!");
+});
+
+const artemisController = require("./controllers/artemisController.js");
+app.use("/pets", artemisController);
+
+
+// 404 PAGE
+app.get("*", (req, res) => {
+  res.status(404).send("Page not found");
+});
+
+// EXPORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
